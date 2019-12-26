b6bba8daa280ef1d5945d00d9651a88cfc349fea
bagang <270262382@qq.com>
2019-12-26 14:59:49 +0800

    优化识别精精准度

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,25 +3,24 @@ echo "Enter release version: "
 read VERSION
 
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  echo "Releasing $VERSION ..."
+echo # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Releasing $VERSION ..."
 
-  npm run build
-  npm run build-lib
-  npm version $VERSION --message "build: $VERSION"
-  git add .
-  git commit -m "$VERSION"
-  git push origin develop
-  # commit
-  git checkout master
-  git merge develop
-  git push origin master
+    npm run build
+    npm run build-lib
+    npm version $VERSION --message "build: $VERSION"
+    git add .
+    git commit -m "$VERSION"
+    git push origin develop
+    # commit
+    git checkout master
+    git merge develop
+    git push origin master
 
-  # publish
-  git tag $VERSION -m "release: $VERSION"
-  git push origin "$VERSION"
+    # publish
+    git tag $VERSION -m "release: $VERSION"
+    git push origin "$VERSION"
 
-  git checkout develop
+    git checkout develop
 fi
