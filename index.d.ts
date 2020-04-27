ba45f57b733db14fc6980d56d53c1bee6be836e8
ldonday <270262382@qq.com>
2020-04-27 10:56:12 +0800

    support npm

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+export = zhAddressParse;
+declare function zhAddressParse (address: string, option?: zhAddressParse.OptionType): zhAddressParse.ParseResult
+declare namespace zhAddressParse{
+    export type ParseResult = {
+        province: string,
+        name: string,
+        city: string,
+        area: string,
+        detail: string,
+        phone: string,
+        postalCode?: string
+    }
+
+    export type OptionType = {
+        type: 0 | 1,
+        textFilter: string,
+        nameMaxLength: number
+    }
+}
